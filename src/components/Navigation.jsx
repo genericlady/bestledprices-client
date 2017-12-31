ff26c15a1fe21cd1eae63ccdd0e218b11855794a
yianna <yiannakokalas@gmail.com>
2017-12-31 10:37:13 -0500

    Update Navigation
    - Add new es6 static proptypes and
      default propTypes.
    - Update Navigation to a black Nav.
    - Remove unused Constructor.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,49 +1,55 @@
 import React from 'react';
+import { func } from 'prop-types';
 
 export default class Navigation extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
+  static defaultProps = {
+    getPrices: () => {},
+    handleChange: () => {},
   }
 
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+  static propTypes = {
+    getPrices: func,
+    handleChange: func,
   }
 
   render() {
     const {
-      getPrices = () => {},
-      handleChange = () => {},
+      getPrices,
+      handleChange,
     } = this.props;
 
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand">Best LED Prices</a>
         <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarColor03"
-          aria-controls="navbarColor03"
+          data-target="#navbarColor01"
+          aria-controls="navbarColor01"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarColor03">
-          {
-            /**
-             * The following <ul> is meant for any links to be added.
-             */
-          }
-          <ul className="navbar-nav mr-auto" />
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav mr-auto">
+            {/** the following code would be the site links.
+              * <li class="nav-item active">
+              *   <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              * </li>
+              * <li class="nav-item">
+              *   <a class="nav-link" href="#">Features</a>
+              * </li>
+              * <li class="nav-item">
+              *   <a class="nav-link" href="#">Pricing</a>
+              * </li>
+              * <li class="nav-item">
+              *   <a class="nav-link" href="#">About</a>
+              * </li>
+              */}
+          </ul>
           <form className="form-inline my-2 my-lg-0">
             <input
               className="form-control mr-sm-2"
