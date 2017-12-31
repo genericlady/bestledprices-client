decedf118b4a06bb9fa7a342a153005d51049ae2
yianna <yiannakokalas@gmail.com>
2017-12-30 21:06:41 -0500

    Update Whitespace in Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -40,10 +40,10 @@ export default class Navigation extends React.Component {
         <div className="collapse navbar-collapse" id="navbarColor03">
           <form className="form-inline my-2 my-lg-0">
             <input
-            className="form-control mr-sm-2"
-            type="text"
-            placeholder="Search"
-            onChange={(event) => handleChange(event)}
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              onChange={(event) => handleChange(event)}
             />
             <button
               onClick={(event) => getPrices(event)}
