b76ec3ef806961eda6d948db18ecda76a07811b3
yianna <yiannakokalas@gmail.com>
2017-12-30 21:04:52 -0500

    Remove unused and empty search component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -23,28 +23,38 @@ export default class Navigation extends React.Component {
     } = this.props;
 
     return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			  <a className="navbar-brand">Best LED Prices</a>
-			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-			    <span className="navbar-toggler-icon"></span>
-			  </button>
-			
-			  <div className="collapse navbar-collapse" id="navbarColor03">
-			    <ul className="navbar-nav mr-auto">
-			    </ul>
-			    <form className="form-inline my-2 my-lg-0">
-          	<input
-              className="form-control mr-sm-2"
-              type="text"
-              placeholder="Search"
-          	  onChange={(event) => handleChange(event)}
-          	/>
-			      <button onClick={(event) => getPrices(event)} className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-            
-			    </form>
-			  </div>
-			</nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand">Best LED Prices</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarColor03"
+          aria-controls="navbarColor03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
+        <div className="collapse navbar-collapse" id="navbarColor03">
+          <form className="form-inline my-2 my-lg-0">
+            <input
+            className="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+            onChange={(event) => handleChange(event)}
+            />
+            <button
+              onClick={(event) => getPrices(event)}
+              className="btn btn-secondary my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     );
   }
 }
