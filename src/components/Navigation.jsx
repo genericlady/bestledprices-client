6c9e9a37313c14b3cea46bb89939e05299973e1c
yianna <yiannakokalas@gmail.com>
2021-06-21 20:16:00 -0700

    Update class to className

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -37,17 +37,17 @@ export default class Navigation extends React.Component {
           <ul className="navbar-nav mr-auto d-none">
             {
               <>
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Features</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
                 </li>
               </>
             }
