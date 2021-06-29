3524d5623d8013446d38c3b6af1f833e68521861
yianna <yiannakokalas@gmail.com>
2021-06-28 21:57:05 -0700

    Refactor Card Component Test further

--- src/components/__tests__/Card.test.tsx
+++ src/components/__tests__/Card.test.tsx
@@ -5,8 +5,6 @@ import { render, cleanup} from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import Card from "../Card";
 
-let container: null | HTMLElement = null;
-
 afterEach(cleanup);
 
 test("Displays information about the item.", () => {
@@ -17,14 +15,12 @@ test("Displays information about the item.", () => {
     href: "https://foobar.com/baz",
     merchant: "Adafruit",
   }
-  act(() => {
-    ({ container } = render(<Card {...props} />));
-  });
+  const { container } = render(<Card {...props} />);
 
-  expect(container!.querySelector(".card-link")!.getAttribute("href")).toBe(props.href);
-  expect(container!.querySelector(".card-img-top")!.getAttribute("src")).toBe(props.image);
-  expect(container!.querySelector(".card-img-top")!.getAttribute("alt")).toBe(props.description);
-  expect(container!.querySelector(".card-title")!.textContent).toBe(props.description);
-  expect(container!.querySelector(".card-price")!.textContent).toBe(props.price);
-  expect(container!.querySelector(".card-merchant")!.textContent).toBe(props.merchant);
+  expect(container.querySelector(".card-link")!.getAttribute("href")).toBe(props.href);
+  expect(container.querySelector(".card-img-top")!.getAttribute("src")).toBe(props.image);
+  expect(container.querySelector(".card-img-top")!.getAttribute("alt")).toBe(props.description);
+  expect(container.querySelector(".card-title")!.textContent).toBe(props.description);
+  expect(container.querySelector(".card-price")!.textContent).toBe(props.price);
+  expect(container.querySelector(".card-merchant")!.textContent).toBe(props.merchant);
 });
